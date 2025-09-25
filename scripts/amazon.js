3becdbd9d77dc39a31cddd09d80eabff51708ed8
codealiirshad <aliirshad4199@gmail.com>
2025-09-25 20:32:52 +0500

    create format currency function & fix radio selectors

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utilities/money.js";
 
 // we used accumaltor pattern (loop through each value in the data and then add it in this variable)
 let productHtml = '';
@@ -25,7 +26,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-price">
-        $${(product.priceCents / 100).toFixed(2)}
+        $${formatCurrency(product.priceCents)}
       </div>
 
       <div class="product-quantity-container">
---
+++ scripts/utilities/money.js
@@ -0,0 +1,4 @@
+// function to format currency
+export function formatCurrency(priceCents) {
+  return (priceCents / 100).toFixed(2);
+};
